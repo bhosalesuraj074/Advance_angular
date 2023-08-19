1565c9a90182367528e06235a77fc7bdc4fc5e46
bhosalesuraj074 <125651189+bhosalesuraj074@users.noreply.github.com>
2023-08-19 16:09:22 +0530

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PipesComponent } from './pipes/pipes.component';
+import { CommonModule } from '@angular/common';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'pipes', component: PipesComponent },
 ];
 @NgModule({
-  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  imports: [BrowserModule, RouterModule.forRoot(routes), CommonModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
   exports: [RouterModule],
--- src/app/pipes/pipes.component.ts
+++ src/app/pipes/pipes.component.ts
@@ -3,13 +3,40 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pipes',
   templateUrl: './pipes.component.html',
-  styleUrls: ['./pipes.component.css']
+  styleUrls: ['./pipes.component.css'],
 })
 export class PipesComponent implements OnInit {
+  employees = [
+    {
+      name: 'John',
+      department: 'Sales',
+      salary: 1000,
+      email: 'john@example.com',
+      phone: '123-456-7890',
+    },
+    {
+      name: 'Jane',
+      department: 'Marketing',
+      salary: 2000,
+      email: 'jane@example.com',
+      phone: '234-567-8901',
+    },
+    {
+      name: 'Dave',
+      department: 'Sales',
+      salary: 1500,
+      email: 'dave@example.com',
+      phone: '345-678-9012',
+    },
+    {
+      name: 'Doe',
+      department: 'Marketing',
+      salary: 2500,
+      email: 'doe@example.com',
+      phone: '456-789-0123',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
