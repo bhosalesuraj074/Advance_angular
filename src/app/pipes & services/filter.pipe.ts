c52cefda92bbda4ea55650c78b92c9f3efd656c3
bhosalesuraj074 <125651189+bhosalesuraj074@users.noreply.github.com>
2023-08-19 16:56:52 +0530

    3 files modified

--- src/app/pipes & services/filter.pipe.ts
+++ src/app/pipes & services/filter.pipe.ts
@@ -7,7 +7,8 @@ export class FilterPipe implements PipeTransform {
   transform(value: any, search: string, end: string): any {
     //console.log('obj', value, '\n s', search);
     let s = value.filter((item: any) => {
-      return item.name.includes(search) && item.name.endsWith(end);
+      let temp = item.name.toLowerCase();
+      return temp.includes(search) && item.name.endsWith(end);
     });
     return s;
   }
