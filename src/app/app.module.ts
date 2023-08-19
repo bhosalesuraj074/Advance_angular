bd175223c1ea36f4052a1f4ac31269e60957bc8a
bhosalesuraj074 <125651189+bhosalesuraj074@users.noreply.github.com>
2023-08-19 16:12:23 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PipesComponent } from './pipes/pipes.component';
-import { CommonModule } from '@angular/common';
+
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'pipes', component: PipesComponent },
 ];
 @NgModule({
-  imports: [BrowserModule, RouterModule.forRoot(routes), CommonModule],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
   exports: [RouterModule],
