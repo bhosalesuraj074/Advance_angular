2c5df758820d34b9fdfef5d446c25b430b0361be
bhosalesuraj074 <125651189+bhosalesuraj074@users.noreply.github.com>
2023-08-19 16:44:58 +0530

    2 files modified

--- src/app/pipes & services/filter.pipe.ts
+++ src/app/pipes & services/filter.pipe.ts
@@ -4,10 +4,10 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'filter',
 })
 export class FilterPipe implements PipeTransform {
-  transform(value: any, search: string): any {
+  transform(value: any, search: string, end: string): any {
     //console.log('obj', value, '\n s', search);
     let s = value.filter((item: any) => {
-      return item.name.includes(search);
+      return item.name.includes(search) && item.name.endsWith(end);
     });
     return s;
   }
