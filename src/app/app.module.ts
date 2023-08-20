362f06fe4a6ff70fa4c2048d994ba8446cebe9ce
bhosalesuraj074 <125651189+bhosalesuraj074@users.noreply.github.com>
2023-08-20 12:15:47 +0530

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,23 @@ import { HomeComponent } from './home/home.component';
 import { PipesComponent } from './pipes/pipes.component';
 import { FormsModule } from '@angular/forms';
 import { FilterPipe } from './pipes & services/filter.pipe';
+import { HooksComponent } from './hooks/hooks.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'pipes', component: PipesComponent },
+  { path: 'hooks', component: HooksComponent },
 ];
 @NgModule({
   imports: [BrowserModule, RouterModule.forRoot(routes), FormsModule],
-  declarations: [AppComponent, PipesComponent, HomeComponent, FilterPipe],
+  declarations: [
+    AppComponent,
+    PipesComponent,
+    HomeComponent,
+    FilterPipe,
+    HooksComponent,
+  ],
   bootstrap: [AppComponent],
   exports: [RouterModule],
 })
