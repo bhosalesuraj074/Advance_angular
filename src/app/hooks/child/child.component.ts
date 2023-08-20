fa2e93cacf45c62eed5b29980a6d4073e802b51a
bhosalesuraj074 <125651189+bhosalesuraj074@users.noreply.github.com>
2023-08-20 20:18:19 +0530

    2 files modified

--- src/app/hooks/child/child.component.ts
+++ src/app/hooks/child/child.component.ts
@@ -1,5 +1,6 @@
 import {
   Component,
+  DoCheck,
   ElementRef,
   Input,
   OnChanges,
@@ -13,18 +14,24 @@ import {
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css'],
 })
-export class ChildComponent implements OnInit, OnChanges {
+export class ChildComponent implements OnInit, OnChanges, DoCheck {
   @Input() childColor: string | undefined;
   @ViewChild('child') color!: ElementRef;
+  // 1 constructor is called first
   constructor() {
-    console.log('Constructor called...');
+    console.log('1. Constructor called...');
   }
+  // 2 ngOnChange excuted when change in Input property decorator
   ngOnChanges(changes: SimpleChanges): void {
-    console.log('ngOnChanges called...');
+    console.log('2. ngOnChanges called...');
     console.log(changes);
   }
-
+  // 3 ngInit excuted once in componet life.
   ngOnInit() {
-    console.log('ngOnInit called');
+    console.log('3. ngOnInit called...');
+  }
+  //4. ngDoCheck excuted every time when the chage is detcted...
+  ngDoCheck(): void {
+    console.log('4. ngDoCheck called...');
   }
 }
