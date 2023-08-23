e96af62e720c85cd23caec054617c4366342b825
bhosalesuraj074 <125651189+bhosalesuraj074@users.noreply.github.com>
2023-08-23 16:48:03 +0530

    1 file modified

--- src/app/pipes & services/rapid.ts
+++ src/app/pipes & services/rapid.ts
@@ -6,7 +6,7 @@ import {
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 export class Rapid implements HttpInterceptor {
@@ -17,7 +17,7 @@ export class Rapid implements HttpInterceptor {
     console.log('from intercepotor 1');
     let header = new HttpHeaders({
       'X-RapidAPI-Key': '142a30b563msh7cb0b0bcbc43ae8p18392djsnb4b09c6a3c22',
-      'X-RapidAPI-Host': 'the-birthday-cake-db.p.rapidapi.',
+      'X-RapidAPI-Host': 'the-birthday-cake-db.p.rapidapi.,com',
     });
     let obj = { msg1: 'This msg from the rapid interceptor 1' };
     let myReq = req.clone({
@@ -26,10 +26,9 @@ export class Rapid implements HttpInterceptor {
     });
     myReq.params;
     return res.handle(myReq).pipe(
-   catchError(error: HttpErrorResponse)=>{
-   }
-
-    )
-
+      catchError((_error: HttpErrorResponse) => {
+        return throwError('USE PROPER URL');
+      })
+    );
   }
 }
