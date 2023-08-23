d46fac0481a6fd43f6be1a9860e1c87123c7844e
bhosalesuraj074 <125651189+bhosalesuraj074@users.noreply.github.com>
2023-08-23 16:43:57 +0530

    1 file modified

--- src/app/pipes & services/rapid.ts
+++ src/app/pipes & services/rapid.ts
@@ -1,24 +1,35 @@
 import {
+  HttpErrorResponse,
+  HttpEvent,
   HttpHandler,
   HttpHeaders,
   HttpInterceptor,
   HttpRequest,
 } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 export class Rapid implements HttpInterceptor {
-  intercept(req: HttpRequest<any>, res: HttpHandler) {
+  intercept(
+    req: HttpRequest<any>,
+    res: HttpHandler
+  ): Observable<HttpEvent<any>> {
     console.log('from intercepotor 1');
     let header = new HttpHeaders({
       'X-RapidAPI-Key': '142a30b563msh7cb0b0bcbc43ae8p18392djsnb4b09c6a3c22',
-      'X-RapidAPI-Host': 'the-birthday-cake-db.p.rapidapi.com',
+      'X-RapidAPI-Host': 'the-birthday-cake-db.p.rapidapi.',
     });
+    let obj = { msg1: 'This msg from the rapid interceptor 1' };
     let myReq = req.clone({
       headers: header,
-      setHeaders: {
-        msg1: 'This msg from the rapid interceptor 1',
-      },
+      setHeaders: obj,
     });
     myReq.params;
-    return res.handle(myReq);
+    return res.handle(myReq).pipe(
+   catchError(error: HttpErrorResponse)=>{
+   }
+
+    )
+
   }
 }
